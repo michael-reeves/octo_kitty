e7dd732238547ac88ccf590e8d0e3ac59dc901b0
Michael Reeves <mreeves68@yahoo.com>
2015-09-08 21:49:06 -0600

    Move StaticPages#index to show and initial page styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get '/auth/github/callback', to: 'sessions#create'
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#show'
 end
