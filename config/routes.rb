c5e9b87ce7b45ea7ba7c5eaa947bb85cdf0b7a93
Michael Reeves <mreeves68@yahoo.com>
2015-09-08 22:21:37 -0600

    add dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/auth/github/callback', to: 'sessions#create'
 
+  get '/dashboard', to: 'dashboard#show'
+
   # You can have the root of your site routed with "root"
   root 'static_pages#show'
 end
