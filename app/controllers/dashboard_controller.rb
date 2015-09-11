a11e769b78343594b75880df3cd1f73b2def90eb
Michael Reeves <mreeves68@yahoo.com>
2015-09-11 01:04:59 -0600

    Add following users to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,14 @@ class DashboardController < ApplicationController
   before_action :authorize!
 
   def show
+    service ||= current_user.service
+    # get the first 3 user that the current user follows
+    # and generate a new service
+    services = service.follows.first(3).map do |follow|
+      Github.new(follow.nickname)
+    end
+
+    # for each service, get the first 5 push requests
+    @follow_pushes = services.map{ |service| service.pushes.first(5) }.flatten
   end
 end
